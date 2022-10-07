de47e67f3f8d19ecb7970f7433f86681a8165000
Therence BRUNE <therence.brune@itsfactory.fr>
2022-10-07 17:15:42 +0200

    WIP more

---
+++ features/Home/More/More.tsx
@@ -0,0 +1,12 @@
+import style from "./More.module.scss";
+
+export const More = () => (
+    <section className={style.container}>
+        <div className={style.textContainer}>
+            <h2 className={style.title}>{"Curious about this site ?"}</h2>
+            <div className={style.skillsContainer}>
+                todo
+            </div>
+        </div>
+    </section>
+);

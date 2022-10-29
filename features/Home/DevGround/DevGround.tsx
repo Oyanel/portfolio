88ff605aa4989d78d33509054456a85f613ed45d
Therence BRUNE <therence.brune@itsfactory.fr>
2022-10-29 22:59:51 +0200

    small updates

--- features/Home/DevGround/DevGround.tsx
+++ features/Home/DevGround/DevGround.tsx
@@ -9,11 +9,11 @@ export const DevGround = () => (
                     <h2 className={style.title}>{"My dev ground"}</h2>
                     <p>
                         {"I’d like to say I am a full stack dev but I mainly create frontend apps." +
-                        " Still, I do some node/java when I need to. Like a lot of devs I do a lot of things." +
+                        " Still, I do some node/java when I need to. Like a lot of devs, I do a lot of things." +
                         " Though I specialize in React, I could probably build an app with any library."}
                     </p>
                     <p>
-                        {" Because sometimes I want to build something, I open a few softwares and try to make it happen." +
+                        {"Because sometimes I want to build something, I open a few softwares and try to make it happen." +
                         " If you which to see some of my work, check this."}
                     </p>
                 </div>
--- features/Home/Me/Me.tsx
+++ features/Home/Me/Me.tsx
@@ -10,8 +10,8 @@ export const Me = () => (
             <h2 className={style.title}>{"Hey It's me, Mar.. Therence"}</h2>
             <div className={style.skillsContainer}>
                 <SkillItem altImage="Computer icon" image={computerIcon} text="I’ve been working in the web for more than 6 years. I started with PHP and then I switched to a more frontend approach in a company developing web apps in the telecom sector." title="I make it" />
-                <SkillItem altImage="scientific tubes icon" image={tubesIcon} inverted text="I enjoy working on personal projects to try new techs and have a different perspective on the web development. I also tried react native, but I prefer web over mobile !" title="I try it" />
-                <SkillItem altImage="remote and ramen icon" image={gameJapIcon} text="I LOVE japanese culture (yes anime too) and video games. Even if I don’t play as much as I used to, I’m still good at Tekken or Valorant." title="I love it" />
+                <SkillItem altImage="Scientific tubes icon" image={tubesIcon} inverted text="I enjoy working on personal projects to try new techs and have a different perspective on the web development. I also tried react native, but I prefer web over mobile !" title="I try it" />
+                <SkillItem altImage="Remote and ramen icon" image={gameJapIcon} text="I LOVE japanese culture (yes anime too) and video games. Even if I don’t play as much as I used to, I’m still good at Tekken or Valorant." title="I love it" />
             </div>
         </div>
     </section>

df017430d236004563f7970d15764644b584c520
Therence BRUNE <therence.brune@itsfactory.fr>
2022-10-29 22:41:39 +0200

    Add gradient to the land page

--- features/Home/Landing/Landing.tsx
+++ features/Home/Landing/Landing.tsx
@@ -2,6 +2,7 @@ import style from "./Landing.module.scss";
 
 export const Landing = () => (
     <section className={style.container}>
+        <div className={style.gradient} />
         <div className={style.textContainer}>
             <h1 className={style.title}>WELCOME</h1>
             <h2 className={style.subTitle}>This is my website, Feel free to explore.</h2>

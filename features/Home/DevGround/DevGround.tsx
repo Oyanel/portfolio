664feec8c653deeea68b8242995a23dd8e822a32
Therence BRUNE <therence.brune@itsfactory.fr>
2022-11-24 14:42:27 +0100

    WIP

--- features/Home/DevGround/DevGround.tsx
+++ features/Home/DevGround/DevGround.tsx
@@ -12,25 +12,30 @@ export const DevGround = () => {
         );
     }
 
+    const selectedStyle = selectedCardIndex !== undefined ? style.isSelected : "";
+
     return (
         <section className={style.section}>
-            <div className={style.container}>
-                <div className={style.textContainer}>
-                    <h2 className={style.title}>{"My dev ground"}</h2>
-                    <p>
-                        {"I’d like to say I am a full stack dev but I mainly create frontend apps." +
-                            " Still, I do some node/java when I need to. Like a lot of devs, I do a lot of things." +
-                            " Though I specialize in React, I could probably build an app with any library."}
-                    </p>
-                    <p>
-                        {"Because sometimes I want to build something, I open a few softwares and try to make it happen." +
-                            " If you which to see some of my work, check this."}
-                    </p>
-                </div>
-                <div className={style.projects}>
-                    <ProjectCard />
-                    <ProjectCard />
-                    <ProjectCard />
+            <div className={style.wrapper}>
+                <div className={`${style.container} ${selectedStyle}`}>
+                    <div className={`${style.textContainer} ${selectedStyle}`}>
+                        <h2 className={style.title}>{"My dev ground"}</h2>
+                        <p>
+                            {"I’d like to say I am a full stack dev but I mainly create frontend apps." +
+                                " Still, I do some node/java when I need to. Like a lot of devs, I do a lot of things." +
+                                " Though I specialize in React, I could probably build an app with any library."}
+                        </p>
+                        <p>
+                            {"Because sometimes I want to build something, I open a few softwares and try to make it happen." +
+                                " If you wish to see some of my work, check this."}
+                        </p>
+                    </div>
+                    <div className={style.projects}>
+                        {projectCardList}
+                    </div>
+                    <div className={`${style.fullText} ${selectedStyle}`}>{"Because sometimes I want to build something, I open a few softwares and try to make it happen." +
+                        " If you wish to see some of my work, check this."}
+                    </div>
                 </div>
             </div>
         </section>
--- features/Home/DevGround/ProjectCard/ProjectCard.tsx
+++ features/Home/DevGround/ProjectCard/ProjectCard.tsx
@@ -4,16 +4,19 @@ import background from "/public/images/backgroundDefault.jpg";
 
 interface IParams {
     isSelected: boolean;
-    onClick(index: number): void;
+    onClick(): void;
 }
 
 export const ProjectCard = (props: IParams) => {
-    const { isSelected } = props;
+    const { isSelected, onClick } = props;
+
+    const selectedStyle = isSelected ? style.isSelected : "";
 
     return (
-        <div className={`${style.project} ${isSelected}`}>
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+        <div className={`${style.project} ${selectedStyle}`} onClick={onClick}>
             <Image alt="b1" layout="fill" objectFit="cover" src={background} />
-            <div className={style.projectCard}>
+            <div className={`${style.projectCard} ${selectedStyle}`}>
                 <div className={style.projectCardText}>
                     <p>Mary b. Decoration</p>
                     <p>A Home designer web site</p>
